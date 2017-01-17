15b352bf7391875cf6cb52b76c54117faed2456a
chenjiabin <1142293069@qq.com>
2017-01-17 08:33:58 +0800

    first

--- routes/wechat.js
+++ routes/wechat.js
@@ -1,15 +1,22 @@
 const router = require('express').Router()
 	, crypto = require('crypto')
+	, wechat = require('wechat')
 
-router.get('/', (req, res)=> {
+router.get('/changeurl', (req, res)=> {
 	var signature = req.query.signature
 		, timestamp = req.query.timestamp
 		, nonce = req.query.nonce
 		, echostr = req.query.echostr
-		, token = gagaga
-		, sha1 = crypto.creatHash('sha1')
-		sskey = [token, timestamp, nonce].sort().join('')
-	
+		, token = 'gagaga'
+		, sha1 = crypto.createHash('sha1')
+		, sha1Str = sha1.update([token, timestamp, nonce].sort().join('')).digest('hex')
+    res.writeHead(200, {'Content-Type': 'text/plain'})
+	res.end((sha1Str === signature) ? echostr : '')
+	return res
+})
+
+router.get('/', (req, res)=> {
+
 })
 
 module.exports = router
