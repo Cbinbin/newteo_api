5bf5d4584b2494baa6a4a47e66c95982873f2786
Cbinbin <1142293069@qq.com>
2017-02-03 01:26:31 +0800

    nothing

--- utils/hosturl.js
+++ utils/hosturl.js
@@ -1,5 +1,5 @@
 module.exports = {
 	local: 'http://cbinbin.xyz/',   //'https://api.newteo.com/',
 	newteo: 'http://cbinbin.xyz/',    //'https://api.newteo.com/',
-	wx: 'http://05275087.ngrok.io/'   //'https://api.newteo.com/'
+	wx: 'http://cbinbin.xyz/'   //'https://api.newteo.com/'
 }
