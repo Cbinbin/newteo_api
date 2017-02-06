c5e77da5473821712083f576d740c5faac5f8fdf
chenjiabin <1142293069@qq.com>
2017-02-07 00:32:20 +0800

    pic

--- routes/wechat.js
+++ routes/wechat.js
@@ -35,7 +35,7 @@ router.post('/', (req, res, next)=> {
 				{
 					res.reply([{ 
 						title: '查看需求', 
-						picurl: `${host.wx}storage/index.jpeg`,
+						picurl: `${host.wx}storage/index2.jpg`,
 						url: `${host.wx}xuqiu`
 					}])
 				} break
@@ -62,7 +62,7 @@ router.post('/', (req, res, next)=> {
 				if(xq.test(message.Content)) {
 					res.reply([{ 
 						title: '查看需求', 
-						picurl: `${host.wx}storage/index.jpeg`,
+						picurl: `${host.wx}storage/index2.jpg`,
 						url: `${host.wx}xuqiu`
 					},
 					{ 
@@ -95,7 +95,7 @@ router.post('/', (req, res, next)=> {
 					if(page <= ye) {
 						res.reply([{ 
 							title: '查看需求', 
-							picurl: `${host.wx}storage/index.jpeg`,
+							picurl: `${host.wx}storage/index2.jpg`,
 							url: `${host.wx}xuqiu`
 						},
 						{ 
@@ -132,51 +132,92 @@ router.post('/', (req, res, next)=> {
 
 
 		} else if(message.MsgType === 'voice') {
-			if(xq.test(message.Recognition)) {
-				res.reply([{ 
-					title: '查看需求', 
-					picurl: `${host.wx}storage/index.jpeg`,
-					url: `${host.wx}xuqiu`
-				}])
-			} else if(djy.test(message.Content)) {
-				var want = String(message.Content.match(/\u7B2C\d{1,2}\u9875/g))    //提取
-					, page = String(want.match(/\d{1,2}/g))
-				if(page <= ye) {
+			Requirement.find({ }, {__v:0})
+			.exec((err, requments)=> {
+				if(err) return res.send(err)
+				requments.map((item)=> {
+					array_rqId.push(item._id)
+					array_rqm.push(`姓名:   ${item.name}, 
+手机号:   ${item.phone}, 
+公司名:   ${item.company}, 
+描述:   ${item.info}, 
+时间:   ${item.create_time}`
+					)
+				})
+				//
+				if((array_rqm.length%5) > 0) { ye = parseInt(array_rqm.length/5) + 1 }
+				else ye = parseInt(array_rqm.length/5)
+				//
+				if(xq.test(message.Recognition)) {
 					res.reply([{ 
 						title: '查看需求', 
-						picurl: `${host.wx}storage/index.jpeg`,
+						picurl: `${host.wx}storage/index2.jpg`,
 						url: `${host.wx}xuqiu`
 					},
 					{ 
-						title: `${array_rqm[(page-1)*5]}`, 
-						url: `${host.wx}requirement/backstage/${array_rqId[(page-1)*5]}`
+						title: `${array_rqm[0]}`, 
+						url: `${host.wx}requirement/backstage/${array_rqId[0]}`
 					},
 					{  
-						title: `${array_rqm[(page-1)*5+1]}`, 
-						url: `${host.wx}requirement/backstage/${array_rqId[(page-1)*5+1]}`
+						title: `${array_rqm[1]}`, 
+						url: `${host.wx}requirement/backstage/${array_rqId[1]}`
 					},
 					{ 
-						title: `${array_rqm[(page-1)*5+2]}`, 
-						url: `${host.wx}requirement/backstage/${array_rqId[(page-1)*5+2]}`
+						title: `${array_rqm[2]}`, 
+						url: `${host.wx}requirement/backstage/${array_rqId[2]}`
 					},
 					{ 
-						title: `${array_rqm[(page-1)*5+3]}`, 
-						url: `${host.wx}requirement/backstage/${array_rqId[(page-1)*5+3]}`
+						title: `${array_rqm[3]}`, 
+						url: `${host.wx}requirement/backstage/${array_rqId[3]}`
 					},
 					{ 
-						title: `${array_rqm[(page-1)*5+4]}`, 
-						url: `${host.wx}requirement/backstage/${array_rqId[(page-1)*5+4]}`
+						title: `${array_rqm[4]}`, 
+						url: `${host.wx}requirement/backstage/${array_rqId[4]}`
 					},
 					{ 
-						title: `           第${page}页                           共${ye}页`, 
+						title: `                              共${ye}页`, 
 						url: ``
 					}])
+				} else if(djy.test(message.Recognition)) {
+					var want = String(message.Recognition.match(/\u7B2C\d{1,2}\u9875/g))    //提取
+						, page = String(want.match(/\d{1,2}/g))
+					if(page <= ye) {
+						res.reply([{ 
+							title: '查看需求', 
+							picurl: `${host.wx}storage/index2.jpg`,
+							url: `${host.wx}xuqiu`
+						},
+						{ 
+							title: `${array_rqm[(page-1)*5]}`, 
+							url: `${host.wx}requirement/backstage/${array_rqId[(page-1)*5]}`
+						},
+						{  
+							title: `${array_rqm[(page-1)*5+1]}`, 
+							url: `${host.wx}requirement/backstage/${array_rqId[(page-1)*5+1]}`
+						},
+						{ 
+							title: `${array_rqm[(page-1)*5+2]}`, 
+							url: `${host.wx}requirement/backstage/${array_rqId[(page-1)*5+2]}`
+						},
+						{ 
+							title: `${array_rqm[(page-1)*5+3]}`, 
+							url: `${host.wx}requirement/backstage/${array_rqId[(page-1)*5+3]}`
+						},
+						{ 
+							title: `${array_rqm[(page-1)*5+4]}`, 
+							url: `${host.wx}requirement/backstage/${array_rqId[(page-1)*5+4]}`
+						},
+						{ 
+							title: `           第${page}页                           共${ye}页`, 
+							url: ``
+						}])
+					} else {
+						res.reply(`总共只有${ye}页`)
+					}
 				} else {
-					res.reply(`总共只有${ye}页`)
+					res.reply('hehe')
 				}
-			} else {
-				res.reply('hehe')
-			}
+			})
 
 
 		} else {
